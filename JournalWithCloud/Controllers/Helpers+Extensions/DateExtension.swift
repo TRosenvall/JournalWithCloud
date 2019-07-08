0e008d1a8a87d707ff5c7f2f7cfa5b1302193497
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-08 15:34:23 -0600

    model controller setup

--- JournalWithCloud/Controllers/Helpers+Extensions/DateExtension.swift
+++ JournalWithCloud/Controllers/Helpers+Extensions/DateExtension.swift
@@ -7,3 +7,14 @@
 //
 
 import Foundation
+
+extension Date {
+    
+    func formatDate () -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .short
+        
+        return formatter.string(from: self)
+    }
+}
--- JournalWithCloud/Controllers/ModelControllers/EntryController.swift
+++ JournalWithCloud/Controllers/ModelControllers/EntryController.swift
@@ -29,12 +29,40 @@ class EntryController {
     }
     
     // Read
-    
+    func fetchEntries () {
+        let database = CloudKitController.sharedInstance.database
+        CloudKitController.sharedInstance.fetchRecordFromCloudKit(type: EntryConstants.typeKey, database: database) { (records, error) in
+            if let error = error {
+                print("Error in \(#function) :  \(error.localizedDescription) /n---/n \(error)")
+            } else if let records = records {
+                let entries = records.compactMap( {Entry(record: $0)} )
+                self.entries = entries
+            }
+        }
+    }
     
     
     // Update
+    func updateEntry (entry: Entry, title: String, body: String) {
+        let database = CloudKitController.sharedInstance.database
+        entry.title = title
+        entry.body = body
+        entry.timestamp = Date()
+        CloudKitController.sharedInstance.updateRecordFromCloudKit(recordID: entry.recordID, database: database, title: title, body: body) { (success) in
+            if success {
+                print("Successfully update entry")
+            }
+        }
+    }
     
     
     // Delete
-    
+    func deleteEntry (entry: Entry) {
+        let database = CloudKitController.sharedInstance.database
+        CloudKitController.sharedInstance.deleteRecordFromCloudKit(recordID: entry.recordID, database: database) { (success) in
+            print("Successfully deleted entry")
+        }
+        guard let entryIndex = entries.firstIndex(of: entry) else {return}
+        entries.remove(at: entryIndex)
+    }
 }
