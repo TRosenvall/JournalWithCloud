669787c570f4032dc403ccf14da297eaeeabae71
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-08 15:34:03 -0600

    model controller setup

---
+++ JournalWithCloud/Controllers/Helpers+Extensions/DateExtension.swift
@@ -0,0 +1,9 @@
+//
+//  DateExtension.swift
+//  JournalWithCloud
+//
+//  Created by Timothy Rosenvall on 7/8/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import Foundation
